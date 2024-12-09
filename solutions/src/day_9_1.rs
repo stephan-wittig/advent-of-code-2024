e7715fa23caa9fe01817cc1f14cab8f94292061f
Stephan Wittig <stephan.wittig@cgi.com>
2024-12-10 00:22:25 +0100

    Solve part 2 really

--- solutions/src/day_9_1.rs
+++ solutions/src/day_9_1.rs
@@ -1,8 +1,15 @@
 use std::{collections::VecDeque, io::BufRead};
 use itertools::repeat_n;
 
+/// Block can either be a file id (usite) or Empty
+#[derive (Clone)]
+enum Block {
+    File(usize),
+    Empty
+}
+
 pub fn run(file: Box<dyn BufRead>) -> Result<(), Box<dyn std::error::Error>> {
-    let mut expanded_fs: VecDeque<char> = VecDeque::new();
+    let mut expanded_fs: VecDeque<Block> = VecDeque::new();
     
     // This should be just one line
     let input = file.lines().next().unwrap().unwrap();
@@ -19,11 +26,8 @@ pub fn run(file: Box<dyn BufRead>) -> Result<(), Box<dyn std::error::Error>> {
             }
             Some(c) => {
                 // Vector with file_index repeated c times
-                let mut file: VecDeque<char> = repeat_n(
-                    match file_index.to_string().chars().nth(0) {
-                        None => {panic!("Cannot stringify file index")}
-                        Some(c) => c
-                    },
+                let mut file: VecDeque<Block> = repeat_n(
+                    Block::File(file_index),
                     c.to_string().parse::<usize>().unwrap()
                 ).collect();
                 expanded_fs.append(&mut file);
@@ -37,8 +41,8 @@ pub fn run(file: Box<dyn BufRead>) -> Result<(), Box<dyn std::error::Error>> {
             }
             Some(c) => {
                 // Vector with '.' repeated c times
-                let mut file: VecDeque<char> = repeat_n(
-                    '.',
+                let mut file: VecDeque<Block> = repeat_n(
+                    Block::Empty,
                     c.to_string().parse::<usize>().unwrap()
                 ).collect();
                 expanded_fs.append(&mut file);
@@ -46,44 +50,46 @@ pub fn run(file: Box<dyn BufRead>) -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    println!("Expanded file system: {:?}", expanded_fs.iter().collect::<String>());
+    let mut cleaned_fs: Vec<usize> = vec![];
 
-    let mut cleaned_fs: Vec<char> = vec![];
-
-    while let Some(next_char) = expanded_fs.pop_front() {
-        if next_char == '.' {
-            if let Some(last_non_space) = get_last_non_space(&mut expanded_fs) {
-                cleaned_fs.push(last_non_space);
-            } else {
-                break; // End loop if empty
+    while let Some(next_block) = expanded_fs.pop_front() {
+        match next_block {
+            Block::Empty => {
+                if let Some(last_non_space) = get_last_non_space(&mut expanded_fs) {
+                    cleaned_fs.push(last_non_space);
+                } else {
+                    break; // End loop if empty
+                }
             }
-        } else {
-            cleaned_fs.push(next_char);
+            Block::File(file) => {
+                cleaned_fs.push(file);
+            }
         }
     }
 
-    println!("Cleaned file system: {:?}", cleaned_fs.iter().collect::<String>());
-
-    let checksum = cleaned_fs.iter().enumerate().fold( 0 as usize,
+    let checksum = cleaned_fs.iter().enumerate().fold( 0,
          | acc, (i, n)| 
         acc + i * n.to_string().parse::<usize>().unwrap()
     );
 
-    println!("Checksum: {}", checksum);
+    println!("\nChecksum: {}", checksum);
 
     Ok(())
 }
 
 /// Get last chat that is not space ('.'). Drops all spaces at the end
-fn get_last_non_space(vd: &mut VecDeque<char>) -> Option<char> {
+fn get_last_non_space(vd: &mut VecDeque<Block>) -> Option<usize> {
     loop {
-        let c = vd.pop_back();
-        match c {
-            Some('.') => {
+        let block = vd.pop_back();
+        match block {
+            Some(Block::Empty) => {
                 continue;
             },
-            r => {
-                return r; // None means the string is empty
+            None => { // None means the string is empty
+                return None; 
+            },
+            Some(Block::File(file)) => {
+                return Some(file);
             }
         }
     }
